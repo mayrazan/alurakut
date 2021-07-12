8785538433e39a19088ca6bb99350b5c5ba1608d
mayrazan <mayramanchein@yahoo.com.br>
2021-07-12 18:41:25 -0300

    First class and github api consumption

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,74 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar({ githubUser }) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = "mayrazan";
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/${githubUser}/followers`)
+      .then((res) => setFollowers(res.data));
+  }, []);
+
+  const reducedArray = followers.slice(0, 6);
+  return (
+    <>
+      <AlurakutMenu githubUser={githubUser} />
+      <MainGrid>
+        {/* <Box style="grid-area: profileArea;"> */}
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+
+            <OrkutNostalgicIconSet confiavel={3} legal={3} sexy={3} />
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({followers.length})
+            </h2>
+
+            <ul>
+              {reducedArray.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <a href={`${item.html_url}`} key={index}>
+                      <img src={`${item.avatar_url}`} />
+                      <span>{item.login}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
